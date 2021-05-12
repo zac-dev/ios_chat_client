4df61a41c117d9877a21294c2d24df5b276e54e0
Zachary BURGESS <zburgess@ford.com>
2021-05-12 17:17:51 +0100

    Initial Commit

---
+++ MyChatApp/MyChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatAppApp.swift
+//  MyChatApp
+//
+//  Created by Zachary Burgess on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
